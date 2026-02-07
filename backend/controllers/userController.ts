3070a2506df918212ae8855c0ce22f31075555cc
logicMac <maclovellates11@gmail.com>
2026-02-08 00:20:33 +0800

    registration commit

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -11,71 +11,86 @@ export const userController = {
         const {username, password} = req.body;
         const users: any = await User.getAllByUsername(username);
 
-        if(!users || users.length === 0) {
-            return res.status(400).json({
-                success: false, 
-                msg:"User not found",
+       try {
+            if(!users || users.length === 0) {
+                return res.status(400).json({
+                    success: false, 
+                    msg:"User not found",
+                });
+            }
+            
+            const user = users[0];
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({
+                    success: false, 
+                    msg:"Invalid credentials"
+                });
+            }
+
+            const token = jwt.sign(
+                {id: user.user_id, username: user.username},
+                process.env.JWT_SECRET as string,
+                { expiresIn: '1h' } 
+            ); 
+
+            return res.status(200).json({
+                success: true,
+                msg: "Login successful",
+                token,
+                user: { id: user.user_id, username: user.username }
+            });
+
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                msg: "Internal Server Error"
             });
         }
-        
-        const user = users[0];
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({
-                success: false, 
-                msg:"Invalid credentials"
-            });
-        }
-
-        const token = jwt.sign(
-            {id: user.user_id, username: user.username},
-            process.env.JWT_SECRET as string,
-            { expiresIn: '1h' } 
-        ); 
-
-        return res.status(200).json({
-            success: true,
-            msg: "Login successful",
-            token,
-            user: { id: user.user_id, username: user.username }
-        });
     },
 
     registerUser: async (req: Request, res: Response) => {
         const {username, password, phone_number, role} = req.body || {};
         const existingUser: any = await User.getAllByUsername(username);
 
-
-        if (existingUser && existingUser.length > 0) {
-            return res.status(400).json({
-                success: false,
-                msg: "Username already exists"
-            });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const registerUser: any = await User.create({
-            username, password: hashedPassword, phone_number, role
-        });
-
-        if (!registerUser) {
-            return res.status(400).json({
-                success: false,
-                msg: "Failed to create user"
-            });
-        }
-
-        res.status(200).json({
-            success: true,
-            msg: "User created Successfully",
-            user: {
-                id: registerUser.insertId,
-                username,
-                phone_number,
-                role
+        try {
+            if (existingUser && existingUser.length > 0) {
+                return res.status(400).json({
+                    success: false,
+                    msg: "Username already exists"
+                });
             }
-        })
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const registerUser: any = await User.create({
+                username, password: hashedPassword, phone_number, role
+            });
+
+            if (!registerUser) {
+                return res.status(400).json({
+                    success: false,
+                    msg: "Failed to create user"
+                });
+            }
+
+            res.status(200).json({
+                success: true,
+                msg: "User created Successfully",
+                user: {
+                    id: registerUser.insertId,
+                    username,
+                    phone_number,
+                    role
+                }
+            })
+
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                msg: "Internal Server Error"
+            });
+        }
     }
 }
 
